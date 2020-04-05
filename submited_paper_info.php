66d30af4fa73c95d8841a31ac82a394e2601765f
STEFANOS CHATZILEFTHERIS <60344302+stefanos03@users.noreply.github.com>
2020-04-05 16:53:48 +0100

    new changes

--- submited_paper_info.php
+++ submited_paper_info.php
@@ -2,207 +2,207 @@
 
 if (!isset($_GET['pid']) || $_GET['pid']=='')
 {
-  header("location: submissions.php");
+    header("location: submissions.php");
 }
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-   require_once("includes/login_module.php");
-   $pageTitle = "Assign User to Project";  
-   require_once("classes/Config.php");
-   require_once("header.php");    
-   
-   
-   $status='';
-
-   $paperid = $_GET['pid'];
-   $pageLink = "assign_reviewer.php?pid=".$paperid;
-   $paper = new Paper();
-   $paperInfo  = $paper->getPaperById($paperid);
-
-   
-   foreach($paperInfo as $result)
-   {
-      $paperTitle = $result["title"];
-      $paperProject = $result['name'];
-      $paperDesc = $result['description'];
-      $paperFile = $result['file'];
-      $paperUserId = $result['userid'];
-      $paperSubmitedby = $result['lastname'].' '.$result['firstname'];
-      $paperDate = new DateTime($result['datesubmitted']);
-      $paperDate = $paperDate->format('l jS F, Y');
-   }
+require_once("includes/login_module.php");
+$pageTitle = "Assign User to Project";
+require_once("classes/Config.php");
+require_once("header.php");
 
 
-   
+$status='';
 
-   if (isset($_POST['submitForm']))
-   {
-        $userid = $_POST['user'];
-        $duration = $_POST['duration'];
+$paperid = $_GET['pid'];
+$pageLink = "assign_reviewer.php?pid=".$paperid;
+$paper = new Paper();
+$paperInfo  = $paper->getPaperById($paperid);
 
 
-        if ($paperid=='' || $userid=='' || $duration=='' )
+foreach($paperInfo as $result)
+{
+    $paperTitle = $result["title"];
+    $paperProject = $result['name'];
+    $paperDesc = $result['description'];
+    $paperFile = $result['file'];
+    $paperUserId = $result['userid'];
+    $paperSubmitedby = $result['lastname'].' '.$result['firstname'];
+    $paperDate = new DateTime($result['datesubmitted']);
+    $paperDate = $paperDate->format('l jS F, Y');
+}
+
+
+
+
+if (isset($_POST['submitForm']))
+{
+    $userid = $_POST['user'];
+    $duration = $_POST['duration'];
+
+
+    if ($paperid=='' || $userid=='' || $duration=='' )
+    {
+        $status='warning';
+        $msg = "All fields are required to be filled before continuing.";
+    }else
+    {
+        $dataArray = array("paperid"=>$paperid,"userid"=>$userid,"duration"=>$duration);
+        $paper = new Paper();
+        $result = $paper->AssignReviewer($dataArray);
+        $status = $result["status"];
+        $msg = $result["msg"];
+    }
+}
+
+
+
+?>
+<br/>
+
+<div class="container">
+    <div class="col-xs-12 text-right">
+        <?php
+        $userRole = '';
+        if ($_SESSION['myRole']=='admin')
         {
-           $status='warning';
-           $msg = "All fields are required to be filled before continuing.";
-        }else
-        {
-            $dataArray = array("paperid"=>$paperid,"userid"=>$userid,"duration"=>$duration);
-            $paper = new Paper();            
-            $result = $paper->AssignReviewer($dataArray);
-            $status = $result["status"];
-            $msg = $result["msg"];
+            $userRole = 'Administrator';
         }
-   }
+        else if ($_SESSION['myRole']=='teamleader')
+        {
+            $userRole = 'Team Leader';
 
-    
+        }else if ($_SESSION['myRole']=='member' || $_SESSION['myRole']=='')
+        {
+            $userRole = 'Member';
+        }
 
-?>  
-        <br/>
-  
-        <div class="container">
-            <div class="col-xs-12 text-right">
-                  <?php
-                           $userRole = '';
-                           if ($_SESSION['myRole']=='admin')
-                           {
-                              $userRole = 'Administrator';
-                           }
-                           else if ($_SESSION['myRole']=='teamleader')
-                           {
-                              $userRole = 'Team Leader';
+        echo "<strong>Welcome ".$_SESSION['myLastname'].' '.$_SESSION['myFirstname']."</strong>,<br>";
+        echo $userRole;
+        ?>
+    </div>
 
-                           }else if ($_SESSION['myRole']=='member' || $_SESSION['myRole']=='')
-                           {
-                              $userRole = 'Member';
-                           }
+    <div class="row">
+        <div class="col-xs-12">
+            <h3 class="text-left price-headline" style="color:purple;">Paper Detailed Information  <small>(Paper Title: <?php echo $paperTitle; ?>)</small></h3>
+        </div>
 
-                           echo "<strong>Welcome ".$_SESSION['myLastname'].' '.$_SESSION['myFirstname']."</strong>,<br>";
-                           echo $userRole;
-                    ?>
-                </div>
 
-            <div class="row">
-                <div class="col-xs-12">
-                    <h3 class="text-left price-headline" style="color:purple;">Paper Detailed Information  <small>(Paper Title: <?php echo $paperTitle; ?>)</small></h3>
-                </div>
+    </div>
 
-                
-            </div>
-                  
-                  <!-- row 1 //-->
-                  <hr>
-             
+    <!-- row 1 //-->
+    <hr>
+
+    <?php
+    require_once("functions/Alert.php");
+
+    ?>
+
+
+
+
+
+
+
+
+
+
+    <div class="row">
+        <div class="col-xs-12">
+            <h4 class="text-left price-headline" style="color:purple;font-weight:bold;">Paper Details</h4>
+        </div>
+
+    </div>
+    <br/>
+    <div class="row" >
+        <div class="col-xs-12">
+            <strong>Project Group</strong>
             <?php
-                  require_once("functions/Alert.php");
+            echo "<br/><i class='fa fa-folder-o'></i> <a href='manageprojects.php'>".$paperProject."</a><br/></br>";
 
             ?>
-           
+        </div>
 
-             
-              
-              
-              
-              
-              
+        <div class="col-xs-12">
+            <strong>Paper Title</strong>
+            <?php
+            echo "<br/><i class='fa fa-file-o'></i> <a href='submissions.php'>".$paperTitle."</a><br/><br/>";
 
+            ?>
+        </div>
+        <div class="col-xs-12">
+            <strong>Description</strong>
+            <?php
+            echo "<br/><i class='fa fa-comment-o'></i> ".$paperDesc."<br/><br/>";
+            ?>
+        </div>
+        <div class="col-xs-12">
+            <Strong>File</Strong>
+            <?php
+            echo "<br/><i class='fa fa-file-o'></i> <a target='_blank' href='uploads/".$paperFile."'>".$paperFile."</a><br/><br/>";
+            ?>
+        </div>
 
-              <div class="row">
-                  <div class="col-xs-12">
-                    <h4 class="text-left price-headline" style="color:purple;font-weight:bold;">Paper Details</h4>
-                </div>
+        <div class="col-xs-12">
+            <Strong>Submitted By</Strong>
+            <?php
+            $code1= "oDdpnVaWwgdsjhMFiyIeLjJjSUCThpJUxfUVwTGnNSGeMLToTq";
+            $code2 = "FoltjKlLKnBdPvQfPQi!oLU!lStPXzTyZomFgktMQluhRbCDHe";
+            echo "<br/><i class='fa fa-user-o'></i> <a target='_blank' href='member.php?mp=".$code1.'-'.$paperUserId.'-'.$code2."'>".$paperSubmitedby."</a><br/><br/>";
+            ?>
+        </div>
 
-              </div>
-              <br/>
-              <div class="row" >
-                  <div class="col-xs-12">
-                      <strong>Project Group</strong>
-                        <?php 
-                            echo "<br/><i class='fa fa-folder-o'></i> <a href='createAndManageProject.php'>" .$paperProject."</a><br/></br>";
-                           
-                        ?>
-                  </div>
-
-                  <div class="col-xs-12">
-                      <strong>Paper Title</strong>
-                        <?php 
-                            echo "<br/><i class='fa fa-file-o'></i> <a href='submissions.php'>".$paperTitle."</a><br/><br/>"; 
-                           
-                        ?>
-                  </div>
-                  <div class="col-xs-12">
-                        <strong>Description</strong>
-                        <?php  
-                            echo "<br/><i class='fa fa-comment-o'></i> ".$paperDesc."<br/><br/>";
-                        ?>
-                  </div>
-                  <div class="col-xs-12">
-                      <Strong>File</Strong>
-                      <?php
-                            echo "<br/><i class='fa fa-file-o'></i> <a target='_blank' href='uploads/".$paperFile."'>".$paperFile."</a><br/><br/>";              
-                      ?>
-                  </div>
-
-                  <div class="col-xs-12">
-                      <Strong>Submitted By</Strong>
-                      <?php
-                          $code1= "oDdpnVaWwgdsjhMFiyIeLjJjSUCThpJUxfUVwTGnNSGeMLToTq";
-                          $code2 = "FoltjKlLKnBdPvQfPQi!oLU!lStPXzTyZomFgktMQluhRbCDHe";
-                            echo "<br/><i class='fa fa-user-o'></i> <a target='_blank' href='member.php?mp=".$code1.'-'.$paperUserId.'-'.$code2."'>".$paperSubmitedby."</a><br/><br/>";              
-                      ?>
-                  </div>
-
-                  <div class="col-xs-12">
-                      <Strong>Date Submitted</Strong>
-                      <?php
-                            echo "<br/><i class='fa fa-calendar-o'></i> ".$paperDate."</a>";              
-                      ?>
-                  </div>
+        <div class="col-xs-12">
+            <Strong>Date Submitted</Strong>
+            <?php
+            echo "<br/><i class='fa fa-calendar-o'></i> ".$paperDate."</a>";
+            ?>
+        </div>
 
 
-              </div>
-             
-              <hr>
+    </div>
 
-              <div class="row">
-                  <div class="col-xs-12">
-                    <h4 class="text-left price-headline" style="color:purple;font-weight:bold;">Assigned Reviewers</h4>
-                </div>
-                  <ol>
-                <?php
-                    $selReviewers = $paper->getReviewersToPaper($paperid);
+    <hr>
 
-                    foreach($selReviewers as $row)
-                    {
-                        $removeAssign = '';
-                        if ($_SESSION['myRole']=='teamleader')
-                        {
-                           $removeAssign = "<a href='#'>Remove</a>";
-                        }
-                        $dateassigned = new DateTime($row['dateassigned']);
-                        $dateassigned = $dateassigned->format('l jS F, Y');
-                        echo "<li><a target='_blank' href='member.php?mp=".$code1.'-'.$row['userid'].'-'.$code2."'>".$row['lastname'].' '.$row['firstname']."</a>  - <small>assigned on ".$dateassigned." &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;".$removeAssign."</small></li>";
+    <div class="row">
+        <div class="col-xs-12">
+            <h4 class="text-left price-headline" style="color:purple;font-weight:bold;">Assigned Reviewers</h4>
+        </div>
+        <ol>
+            <?php
+            $selReviewers = $paper->getReviewersToPaper($paperid);
 
-
-                    }
-
-                ?>
-                  </ol>
-              </div>
-              <br/>
-              <hr>
+            foreach($selReviewers as $row)
+            {
+                $removeAssign = '';
+                if ($_SESSION['myRole']=='teamleader')
+                {
+                    $removeAssign = "<a href='#'>Remove</a>";
+                }
+                $dateassigned = new DateTime($row['dateassigned']);
+                $dateassigned = $dateassigned->format('l jS F, Y');
+                echo "<li><a target='_blank' href='member.php?mp=".$code1.'-'.$row['userid'].'-'.$code2."'>".$row['lastname'].' '.$row['firstname']."</a>  - <small>assigned on ".$dateassigned." &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;".$removeAssign."</small></li>";
 
 
-              
-                          
-    </div><!-- end of container //--> 
+            }
 
-     
-  
+            ?>
+        </ol>
+    </div>
+    <br/>
+    <hr>
 
-    
+
+
+
+</div><!-- end of container //-->
+
+
+
+
+
 
 <?php
-   require_once("footer.php");
+require_once("footer.php");
 
 ?>
--- submitpaper_by_member1.php
+++ submitpaper_by_member1.php
@@ -3,11 +3,11 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
    require_once("includes/login_module.php");
-   $pageTitle = "Assign User to Project";  
+   $pageTitle = "Assign User to Project";
    require_once("classes/Config.php");
-   require_once("header.php");    
-   
-   
+   require_once("header.php");
+
+
    $status='';
 
 
@@ -22,7 +22,7 @@ ini_set('display_errors', 1);
         $title = $_POST['title'];
         $description = $_POST['description'];
 
-        
+
         if ($projectid=='' || $title=='' || $description=='' || $_SESSION['fileUpload']==0)
         {
            $status='warning';
@@ -30,7 +30,7 @@ ini_set('display_errors', 1);
         }else
         {
             $dataArray = array("projectid"=>$projectid,"title"=>$title,"description"=>$description,"file"=>$_SESSION['uploadedFile'],"submitedby"=>$_SESSION['myUserId']);
-            $paper = new Paper();            
+            $paper = new Paper();
             $result = $paper->submitPaper($dataArray);
             $status = $result["status"];
             $msg = $result["msg"];
@@ -45,9 +45,9 @@ ini_set('display_errors', 1);
         $description = $_POST['description'];
    }
 
-    
 
-?>  
+
+?>
         <br/>
 
         <div class="container">
@@ -77,24 +77,24 @@ ini_set('display_errors', 1);
                     <h3 class="text-left price-headline" style="color:purple;">Paper Submission <small>(Upload a research paper)</small></h3>
                 </div>
 
-                
+
             </div>
-                  
+
                   <!-- row 1 //-->
                   <hr>
-             
+
             <?php
                   require_once("functions/Alert.php");
 
             ?>
-           
+
 
              <form name="uploadpaper" action="submitpaper_by_member1.php" method="post" enctype="multipart/form-data">
-              
+
               <div class="form-group row">
-                  
+
                   <label for="Project Name"  class="col-xs-12 col-sm-2 col-form-label text-right">Project</label>
-                  
+
                   <div class="form-group col-xs-12 col-sm-5">
                       <select class="form-control" name="project">
                             <option></option>
@@ -112,34 +112,34 @@ ini_set('display_errors', 1);
                                 {
                                   $selected = 'selected';
                                 }
-                          
+
                             ?>
                             <option <?php echo $selected; ?> value="<?php echo $id; ?>"><?php echo $name; ?></option>
-                             
+
 
                             <?php
 
                               }
 
-                            ?>   
+                            ?>
                       </select>
                   </div>
               </div>
 
               <div class="form-group row">
-            
+
                    <label for="Project Name" class="col-xs-12 col-sm-2 col-form-label text-right">Title</label>
-                      
+
                     <div class="col-xs-12 col-sm-10">
-                        
-                            <input class="form-control" type="text" name="title" value="<?php echo $title; ?>"/>                     
-                    </div> 
+
+                            <input class="form-control" type="text" name="title" value="<?php echo $title; ?>"/>
+                    </div>
 
               </div>
               <div class="form-group row">
-                  
+
                   <label for="Project Short Name"  class="col-xs-12 col-sm-2 col-form-label text-right">Description</label>
-                  
+
                   <div class="col-xs-12 col-sm-8">
                       <textarea class="form-control" cols="80" rows="5" name="description"><?php echo  $description; ?></textarea>
                   </div>
@@ -155,24 +155,24 @@ ini_set('display_errors', 1);
                             require_once("uploadfile.php");
                             echo "</strong><br/><br/>";
 
-                          }                  
+                          }
                       ?>
                   </div>
               </div>
 
               <div class="form-group row">
-                  
+
                   <label for="Project Short Name"  class="col-xs-12 col-sm-2 col-form-label text-right">File</label>
-                  
+
                   <div class="col-xs-7 col-sm-5">
                       <input type="file" name="fileToUpload" >
                       <input type="submit" name="uploadFile" value="Upload File" class="btn btn-default btn-sm">
                   </div>
-                  
+
               </div>
-                            
+
               <div class="row" style="margin-top:10px;">
-                  
+
                   <div class="col-xs-2 col-sm-2">&nbsp;</div>
                   <div class="col-xs-10 col-sm-10">
                       <input  class="btn btn-primary" type="submit" name="submitForm" value="Submit Paper"/>
@@ -208,28 +208,28 @@ ini_set('display_errors', 1);
               </div>
               <br/>
               <?php
-                  
+
                   foreach($list as $row)
                   {
                     $datesubmitted = new DateTime($row['datesubmitted']);
                     $datesubmitted = $datesubmitted->format('l jS F, Y');
-                    
+
               ?>
               <div class="row" >
                   <div class="col-xs-4">
-                        <?php 
+                        <?php
                             echo "<i class='fa fa-folder-o'></i> <a href='createAndManageProject.php'>" .$row['name']."</a><br/>";
                             echo "<small>Submitted on ".$datesubmitted."</small>";
                         ?>
                   </div>
                   <div class="col-xs-4">
-                        <?php  
+                        <?php
                             echo "<i class='fa fa-comment-o'></i> <a href='submited_paper_info.php?pid=".$row['id']."'>".$row['title']."</a>";
                         ?>
                   </div>
                   <div class="col-xs-4">
                       <?php
-                            echo "<i class='fa fa-file-o'></i> <a target='_blank' href='uploads/".$row['file']."'>".$row['file']."</a>";              
+                            echo "<i class='fa fa-file-o'></i> <a target='_blank' href='uploads/".$row['file']."'>".$row['file']."</a>";
                       ?>
                   </div>
 
@@ -241,13 +241,13 @@ ini_set('display_errors', 1);
               <?php
                   }
               ?>
-                          
-    </div><!-- end of container //--> 
 
-     
-  
+    </div><!-- end of container //-->
 
-    
+
+
+
+
 
 <?php
    require_once("footer.php");
